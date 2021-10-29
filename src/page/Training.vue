376a2344223af0e93a5bb25862d9c21487908436
Yi Ding <yiding215@126.com>
2021-10-30 00:11:03 +0200

    给马加名字

--- src/page/Training.vue
+++ src/page/Training.vue
@@ -1,8 +1,33 @@
 <template>
     <v-container fluid>
+<!--        <v-list>-->
+<!--            <v-list-group-->
+<!--                    v-for="(item, i) in horseList"-->
+<!--                    :key= i-->
+<!--                    v-model="item.active"-->
+<!--                    :prepend-icon="item.action"-->
+<!--                    no-action-->
+<!--            >-->
+<!--                <template v-slot:activator>-->
+<!--                    <v-list-item-content>-->
+<!--                        <v-list-item-title v-text="item.title"></v-list-item-title>-->
+<!--                    </v-list-item-content>-->
+<!--                </template>-->
+
+<!--                <v-list-item-->
+<!--                        v-for="child in item.items"-->
+<!--                        :key="child.title"-->
+<!--                >-->
+<!--                    <v-list-item-content>-->
+<!--                        <v-list-item-title v-text="child.title"></v-list-item-title>-->
+<!--                    </v-list-item-content>-->
+<!--                </v-list-item>-->
+<!--            </v-list-group>-->
+<!--        </v-list>-->
+
         <v-row>
             <v-col
-                    v-for="(item, n) in horsesList"
+                    v-for="(item, n) in horseList"
                     :key="n"
                     class="d-flex child-flex"
                     cols="4"
@@ -10,11 +35,6 @@
                 <v-card >
                     <v-img height="88" :src=item.avatar></v-img>
                     <v-spacer/>
-                    <v-radio
-                            :label=item.name
-                            :value="1"
-                    ></v-radio>
-                        <v-spacer/>
                 </v-card>
             </v-col>
         </v-row>
@@ -23,79 +43,19 @@
 </template>
 
 <script>
+    import {myHorseList} from "../modules/moduleIndex";
+
     export default {
         name: "Training",
 
         data() {
             return {
-                horsesList: [
-                    {
-                        id: 1,
-                        name: "Eminy",
-                        avatar: "http://i2.itc.cn/20180829/36b3_51604eaf_5988_b9aa_4f22_b8d9c4c2299c_1.jpg",
-                        pedigreeId: 0, //种族
-                        pedigree: "冰岛马", //种族
-                        pedigreeCharacterIds: [], //特性列表
-                        pedigreeCharacterName: ["比较平庸","耐力好","比较温顺"], //特性名称
-                        skillIds: [], //技巧列表
-                        skillName: [], //技巧名称
-                        curCourage: 0,
-                        curSportAbility: 0,
-                        curAccurateAbility: 0,
-                        curSpeedAbility: 0,
-                        curCooperateAbility: 0,
-                    },
-                    {
-                        id: 2,
-                        name: "Amy",
-                        avatar: "https://img3.jiemian.com/101/original/20181023/154026091835642000_a640x364.jpg",
-                        pedigreeId: 0, //种族
-                        pedigree: "汉诺威马", //种族
-                        pedigreeCharacterIds: [], //特性列表
-                        pedigreeCharacterName: ["强壮温和"], //特性名称
-                        skillIds: [], //技巧列表
-                        skillName: [], //技巧名称
-                        curCourage: 0,
-                        curSportAbility: 0,
-                        curAccurateAbility: 0,
-                        curSpeedAbility: 0,
-                        curCooperateAbility: 0,
-                    },
-                    {
-                        id: 3,
-                        name: "Linda",
-                        avatar: "https://ichef.bbci.co.uk/news/640/cpsprodpb/EA7F/production/_119713006_27999236-21fc-44c2-84a5-8be0eef78ceb.jpg",
-                        pedigreeId: 0, //种族
-                        pedigree: "阿拉伯马", //种族
-                        pedigreeCharacterIds: [], //特性列表
-                        pedigreeCharacterName: ["极为劣性","亢奋易冲动"], //特性名称
-                        skillIds: [], //技巧列表
-                        skillName: [], //技巧名称
-                        curCourage: 0,
-                        curSportAbility: 0,
-                        curAccurateAbility: 0,
-                        curSpeedAbility: 0,
-                        curCooperateAbility: 0,
-                    },
-                    {
-                        id: 4,
-                        name: "Feya",
-                        avatar: "http://photocdn.sohu.com/20080818/Img258957504.jpg",
-                        pedigreeId: 0, //种族
-                        pedigree: "帕索菲诺", //种族
-                        pedigreeCharacterIds: [], //特性列表
-                        pedigreeCharacterName: ["小巧灵活"], //特性名称
-                        skillIds: [], //技巧列表
-                        skillName: [], //技巧名称
-                        curCourage: 0,
-                        curSportAbility: 0,
-                        curAccurateAbility: 0,
-                        curSpeedAbility: 0,
-                        curCooperateAbility: 0,
-                    },
+            }
+        },
 
-
-                ]
+        computed: {
+            horseList() {
+                return myHorseList
             }
         }
     }
