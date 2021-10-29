3564fad71c5b37adaf54b2de308e2409c90e8d97
Haodong Ju <juhaodong@gmail.com>
2021-10-29 15:00:30 +0200

    Update GameRender.vue

--- src/page/GameRender.vue
+++ src/page/GameRender.vue
@@ -10,7 +10,7 @@
           <v-card :key="i"
                   style="width: 100%;height: 100%"
                   v-for="i in mapSizeX*mapSizeY">
-            <template v-if="mapInfo[i]">{{ mapInfo[i] }}</template>
+            <template v-if="mapInfo[i]>0">{{ mapInfo[i] }}</template>
           </v-card>
         </div>
       </div>
@@ -43,30 +43,57 @@ export default {
     fromXYToIndex(x, y, size) {
       return y * size + x
     },
-    getNextPossibleSlot(currentIndex = 0, currentCount = 1) {
-
-      const direction = getRandomInt(8)
-      const [x, y] = this.fromIndexToXY(direction, 3, 1)
-      const [currentX, currentY] = this.fromIndexToXY(currentIndex,
-          this.mapSizeY, 0)
-      console.log(currentX, currentY, 'currentIndex')
-      const mapLimit = this.mapSizeY
+    slotIsPossible(slotIndex) {
       const fitInBounds = function (number, bounds) {
         return number >= 0 && number <= bounds
       }
-      const [afterX, afterY] = [currentX + x, currentY + y]
-      console.log(afterX,afterY,'afterIndex')
-      if (fitInBounds(afterX, mapLimit) && fitInBounds(afterY, mapLimit)) {
-        const slotCount = this.fromXYToIndex(afterX, afterY, mapLimit)
-        if (this.mapInfo[slotCount]) {
-          return this.getNextPossibleSlot(currentIndex, currentCount)
+      const mapLimit = this.mapSizeY
+      let [x, y] = this.fromIndexToXY(slotIndex, mapLimit)
+      if (fitInBounds(x, mapLimit) && fitInBounds(y, mapLimit)) {
+        if (this.mapInfo[slotIndex]) {
+          return false
         } else {
-          this.mapInfo[slotCount] = currentCount
-          return slotCount
+          return true
         }
       } else {
-        return this.getNextPossibleSlot(currentIndex, currentCount)
+        return false
       }
+    },
+    getNextPossibleSlot(currentIndex = 0, currentCount = 1) {
+
+      const direction = Math.floor(getRandomInt(3))*2+1
+      console.log(direction)
+      const moveToDirection = (direction, currentIndex) => {
+        const [x, y] = this.fromIndexToXY(direction, 3, 1)
+        const [currentX, currentY] = this.fromIndexToXY(currentIndex,
+            this.mapSizeY, 0)
+        const [afterX, afterY] = [currentX + x, currentY + y]
+        return [afterX, afterY]
+      }
+
+      const [afterX, afterY] = moveToDirection(direction, currentIndex)
+      const index = this.fromXYToIndex(afterX, afterY, this.mapSizeY)
+      const setIndex = (index, count) => {
+        this.$set(this.mapInfo, index, count)
+      }
+
+
+      if (this.slotIsPossible(index)) {
+        setIndex(index, currentCount)
+        return index
+      } else {
+        for (let i = 3; i >0; i--) {
+          const [afterX, afterY] = moveToDirection(i*2+1, currentIndex)
+          const index = this.fromXYToIndex(afterX, afterY, this.mapSizeY)
+          if (this.slotIsPossible(index)) {
+            console.log(index, 'this index is possible')
+            setIndex(index, currentCount)
+            return index
+          }
+        }
+        return -1
+      }
+
     },
 
   },
@@ -75,6 +102,9 @@ export default {
     let slotIndex = 0
     while (i < 96) {
       slotIndex = this.getNextPossibleSlot(slotIndex, i)
+      if (slotIndex < 0) {
+        break
+      }
       i++
     }
     console.log(this.mapInfo)
